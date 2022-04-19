327e9a666b11317ecb76e9fad6e637dbf66df229
Shubham Mishra <shubhammishra78640@gmail.com>
2022-04-19 11:05:09 +0530

    updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Headerslider from "./components/Headerslider";
 import Header from './components/Header';
-import Product from './components/Product';
+import {Product, Product1} from './components/Product';
 function App() {
   return (
     <div className="App">
      <Header/>
     <Headerslider/>
     <Product/>
+    <Product1/>
     </div>
   );
 }
--- my-app/src/components/Product.jsx
+++ my-app/src/components/Product.jsx
@@ -38,4 +38,47 @@ function Product() {
   );
 }
 
-export default Product;
+
+
+function Product1() {
+    var data1=["https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/062cea23-9a6a-44b9-bdd4-87cae6a462311645602543339-Kurta-sets.jpg",
+    "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/8d65d400-decd-4f42-902c-a40350a16ed11645602543346-Kurtas.jpg",
+    "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/398ee53b-5899-4a9a-9d0b-b35d60c01cb41645602543325-Dresses.jpg",
+    "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/2f410d81-7fae-400e-9ecc-b4a8b6df72b91645602543430-Women-Jeans.jpg",
+    "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/82a9be01-3032-4725-9500-bcc94366b7931645602543399-Mens-Shirts.jpg",
+    "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/c6b09c0f-5c57-472c-a3fc-854ec506a90e1645602543387-Men-T-shirt.jpg",
+    "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/f08e2bac-9bed-4f87-b022-0dce8defeded1645602543380-Men-Trousers.jpg"
+
+
+]
+
+  return (
+      <>
+      <h1 className="product-deal-heading-2">BEST OF MYNTRA EXCLUSIVE BRANDS
+</h1>
+    <div className="Product-container-2">
+        
+
+{
+         data1.map(item=>{
+           return(  
+           <img
+             className="product-img-2"
+             src={item}
+             alt=""
+             
+             
+           />
+         
+         
+           )
+         })
+       }
+    </div>
+    </>
+  );
+}
+
+
+
+export { Product,Product1};
