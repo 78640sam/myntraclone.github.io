8851a8ee2324d5688b5e6186ccb07fdb2addd10b
Shubham Mishra <shubhammishra78640@gmail.com>
2022-04-19 11:30:08 +0530

    updated

---
+++ my-app/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import "./style.css";
+//import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+function Footer() {
+
+  return (
+    <div className="footer-container">
+    
+    <div>
+      
+    </div>
+    
+  
+    </div>
+  );
+}
+
+export default Footer;
--- my-app/src/components/Headerslider.jsx
+++ my-app/src/components/Headerslider.jsx
@@ -24,6 +24,26 @@ function Headerslider() {
               <img src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/4/18/ef3b8626-d983-4330-889c-cf3cc63014951650286485393-luxe-beauty-fest-desktop--1---1-.jpg" 
               alt='' />
           </div>
+          <div>
+              <img src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/4/18/ef3b8626-d983-4330-889c-cf3cc63014951650286485393-luxe-beauty-fest-desktop--1---1-.jpg" 
+              alt='' />
+          </div>
+          <div>
+              <img src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/4/18/a1823fba-458c-46fd-989d-c3446cb094e21650287212131-Desktop--1-.jpg" 
+              alt='' />
+          </div>
+          <div>
+              <img src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/4/16/600e35a8-9a79-438e-a46d-09baf2e4b6f81650126465942-T-Shirts_Desk.jpg" 
+              alt='' />
+          </div>
+          <div>
+              <img src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/4/9/ff085daa-289c-48ab-aa01-4bfb90d1e2281649528601945-Loreal-Pro_Desk_Baner--2-.jpg" 
+              alt='' />
+          </div>
+          <div>
+              <img src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/4/9/90f7c989-c319-4472-95a8-2619d90c31e31649528657708-Premium-Watches_Dk.jpg" 
+              alt='' />
+          </div>
       </Carousel>
      
     </div>
