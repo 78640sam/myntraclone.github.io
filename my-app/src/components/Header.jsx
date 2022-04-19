474ccdaf2e582db6e20ab92950bd6812d2332ea6
Shubham Mishra <shubhammishra78640@gmail.com>
2022-04-19 10:03:24 +0530

    update

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import "./style.css";
+function Header() {
+
+  return (
+    <div className="Header-container">
+     <div>
+         <img className="header-img" src="https://imgs.search.brave.com/8Qby7hAAAQPG9uBC4ziL7oxSEUDIPETL06LmAU7OnQs/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC44/Tkd4am9hbUZQb21T/VERGREYxTGVRSGFI/YSZwaWQ9QXBp" alt=""/>
+     </div>
+     <div className="header-name">
+         <h3>MEN</h3>
+         <h3>WOMEN</h3>
+         <h3>KIDS</h3>
+         <h3>HOME & LIVING</h3>
+         <h3>BEAUTY</h3>
+         <h3>STUDIO</h3>
+     </div>
+     <div>
+         <input className="header-input" type="search"/>
+     </div>
+     <div></div>
+  
+    </div>
+  );
+}
+
+export default Header;
--- my-app/src/components/Headerslider.jsx
+++ my-app/src/components/Headerslider.jsx
@@ -3,6 +3,7 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 
 function Headerslider() {
+  
   return (
     <div className="">
     
@@ -16,11 +17,11 @@ function Headerslider() {
               alt='' />
           </div>
           <div>
-              <img src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/4/9/d88bf465-8b71-489b-99f6-311c832b89ad1649528433105-Kurtas---Sets_Dk.jpg" 
+              <img src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/4/16/600e35a8-9a79-438e-a46d-09baf2e4b6f81650126465942-T-Shirts_Desk.jpg" 
               alt='' />
           </div>
           <div>
-              <img src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/4/9/d88bf465-8b71-489b-99f6-311c832b89ad1649528433105-Kurtas---Sets_Dk.jpg" 
+              <img src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/4/18/ef3b8626-d983-4330-889c-cf3cc63014951650286485393-luxe-beauty-fest-desktop--1---1-.jpg" 
               alt='' />
           </div>
       </Carousel>
