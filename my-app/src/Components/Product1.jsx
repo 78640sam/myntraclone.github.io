7a6cdce3460e8332ee8ea7796212c2e4abe3dd02
Shubham Mishra <shubhammishra78640@gmail.com>
2022-05-01 12:35:07 +0530

    updated home page

--- my-app/src/Components/Product1.jsx
+++ my-app/src/Components/Product1.jsx
@@ -1,6 +1,6 @@
 import "./style.css";
 function Product3() {
-    var data=["https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/4/18/bdca38e1-6073-44e4-95a4-661ccb74a8e01650300879505-Polo-T-Shirts--1-.jpg",
+    var data=["https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/4/21/96ae746e-e33d-4f99-9594-0cff65fde0521650542579795-W-_Biba.jpg",
 
  "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/4/18/e2b52463-3ec4-4043-a3ee-9b89287a3e5a1650300879520-Sarees--1-.jpg",
  "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/4/18/8b1905c6-e142-46d3-8c8c-67e5b521a8cf1650300984767-Luxe-Beauty.jpg",
@@ -9,6 +9,7 @@ function Product3() {
 "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/4/18/d5cd5ffb-6c64-4a63-b2bb-30eef5490e1e1650300879512-Shorts---Skirts.jpg",
 "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/4/18/41117fa4-7871-40ed-bed4-b482521d6d5d1650300879527-Activewear.jpg"
 ]
+
 
   return (
       <>
